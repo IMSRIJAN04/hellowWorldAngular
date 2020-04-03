415c2508a5bed388b0fb68adba8e6c88ffae41fa
IMSRIJAN04 <srijanerroju@gmail.com>
2020-04-03 17:56:12 +0530

    adding the test.component.html to app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Welome to the Angular World';
+  title = 'Angular World';
 }
